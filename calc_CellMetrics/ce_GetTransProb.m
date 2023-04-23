81a353e2b813959b3002b203b2694517695720a5
Emily Aery Jones <emily.aster.jones@gmail.com>
2023-04-23 10:28:17 -0700

    Adding corrections for baseline and prior FR

--- calc_CellMetrics/ce_GetTransProb.m
+++ calc_CellMetrics/ce_GetTransProb.m
@@ -1,4 +1,4 @@
-function [trans,prob,prob_uncor,pred] = ce_GetTransProb(rawCCG,n,binSize,conv_win,varargin)
+function [trans, trans_fr_corr, trans_prior_corr] = ce_GetTransProb(rawCCG,n,fr,binSize,conv_win,varargin)
 % Extract the baseline corrected CCG + spike trans probe from raw CCG
 
 %rawCCG = spike count between reference and target spike train
@@ -16,24 +16,26 @@ else
     intwin = round(length(rawCCG)/2) + round([.0008:binSize:.0048]/binSize);
 end
 
-%get CCG normalized by number of reference spikes
-prob_uncor = rawCCG./n;
-
-%calculate low freq. network comodulation
+%subtract low freq. network comodulation and normalize to # reference spikes
 [ ~, pred ] = ce_cch_conv( rawCCG, round(conv_win/binSize));
-
-%baseline subtract
 prob = (rawCCG(:) - pred)/n;
-
-prob1 = prob;
-prob1(prob1<0) = 0;
+prob(prob<0) = 0;
 
 %integrate
-trans = sum(prob1(intwin));
+trans = sum(prob(intwin));
 
 % EAJ added 4/19/2023: deal with cases where there are >1 spikes in the
 % integration window (so probability can't exceed 1)
-if trans>1
-    trans = 1;
-end
+trans = min(trans, 1);
+
+%repeat with: subtract baseline FR
+prob = rawCCG(:)/n;
+trans_fr_corr = sum(prob(intwin)) - fr*(.0048-.0008);
+trans_fr_corr = min(trans_fr_corr, 1);
+
+%repeat with: subtract firing rate prior to reference spike
+pre_intwin = round(length(rawCCG)/2) - round([.0008:binSize:.0048]/binSize);
+trans_prior_corr = sum(prob(intwin)) - sum(prob(pre_intwin));
+trans_prior_corr = min(trans_prior_corr, 1);
+
 end
